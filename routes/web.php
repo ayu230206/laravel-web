e2ca5435c7d8263a9c7ae721b07e6378ff9997aa
ayu230206 <ayu24ti@mahasiswa.pcr.ac.id>
2025-10-01 20:46:01 +0700

    Latihan Passing Data dan Git

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MahasiswaController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PegawaiController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/pcr', function () {
+    return 'Selamat Datang di Website Kampus PCR!';
+});
+
+Route::get('/nama/{param1}', function ($param1) {
+    return 'Nama saya: '.$param1;
+});
+
+Route::get('/mahasiswa', function () {
+    return 'Halo Mahasiswa';
+})->name('mahasiswa.show');
+
+Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
+
+Route::get('/about', function () {
+    return view('halaman-about');
+});
+
+Route::get('home', [HomeController::class,'index']);
+
+Route::get('/halaman-pegawai', [PegawaiController::class,'index']);
